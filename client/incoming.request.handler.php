68e44efbcd38c31198a8a85cc04194eda974a6af
mehdi <mehdone@gmail.com>
2013-02-27 17:59:30 +1100

    Minor changes.

--- client/incoming.request.handler.php
+++ client/incoming.request.handler.php
@@ -23,9 +23,6 @@
 session_start();
 ini_set( "display_errors", 1 );
 
-// SET DEFAULT TIMEZONE
-date_default_timezone_set( "Asia/Tehran" );
-
 // THIS IS PHP SCRIPT THAT HOLDS ALL DEFINED CONSTANTS
 // ABOUT THIS WHOLE WEBSITE.
 require_once "../config.php";
